476b647af7a04a5373f20c59831b2576b5758edd
Marwane GROSJACQUES <marwane.grosjacques@ens.uvsq.fr>
2021-04-11 18:36:12 +0200

    a

--- Tapatan.py
+++ Tapatan.py
@@ -1,12 +1,5 @@
 # Marwane Grosjacques
 # Eryne Guenet
-<<<<<<< HEAD
-<<<<<<< HEAD
 # Lou Delattre
-=======
 # Sarah Louis El Khoury
->>>>>>> de451d765307323987086b5952ced197421d4b75
-=======
-# Sarah Louis El Khoury
-# Alexandre Mihet (test)
->>>>>>> 34c31c786b8f8024fd9fa952453d4761a3bf8d28
+# Alexandre Mihet
