ba627554a50a44f38796b362d2d29bc3318f1034
Eryne GUENET <eryne.guenet@ens.uvsq.fr>
2021-04-11 18:46:08 +0200

    interface

--- Tapatan.py
+++ Tapatan.py
@@ -4,3 +4,40 @@
 # Sarah Louis El Khoury
 # Alexandre Mihet
 # Emma le roy pardonche
+
+
+
+import tkinter as tk
+
+
+
+# Création de la fenêtre principale
+Mafenetre = tk.Tk()
+Mafenetre.title("Jeu du Tapatan")
+# Création d'un widget Canvas
+Largeur = 300
+Hauteur = 300
+Canevas = tk.Canvas(Mafenetre, width = Largeur, height =Hauteur, bg ="white")
+
+Canevas.bind()
+Canevas.pack(padx =5, pady =5)
+Canevas.create_line(0,150,300,150,fill="black",width=4)
+
+Canevas.create_line(0,300,300,0,fill="black",width=4)
+
+Canevas.create_line(150,300,150,0,fill="black",width=4)
+
+Canevas.create_line(0,0,300,300,fill="black",width=4)
+
+Canevas.create_line(0,4,300,4,fill="black",width=4)
+
+Canevas.create_line(4,4,4,300,fill="black",width=4)
+
+Canevas.create_line(300,0,300,300,fill="black",width=4)
+
+Canevas.create_line(0,300,300,300,fill="black",width=4)
+
+
+tk.Button(Mafenetre)
+
+Mafenetre.mainloop()
